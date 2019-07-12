cd8afcaa0d671b12b8302de692fc8af32afc7be8
cpozos <38520814+cpozos@users.noreply.github.com>
2019-07-12 09:16:01 -0500

    Uso nuevas características en el tes.
    Se hace uso de las nuevas características en el test.js.

--- test.js
+++ test.js
@@ -1,9 +1,37 @@
-function callback(valorMonto){
-    console.log('Monto : ' + valorMonto);
+
+/* CALLBACKS */
+// Funciones que se ejecuta cuando existe un cambio en el valor del input
+// Se les pasa como parámetro el nuevo valor en string.
+
+function calcularCambio(nuevoValorEnInput)
+{
+    console.log('Calculando cambio - valor: ' + nuevoValorEnInput );
 }
 
+function imprimirPantalla(nuevoValorEnInput)
+{
+    console.log('imprimiendo en pantalla - valor: ' + nuevoValorEnInput);
+}
 
-MASCARA_CONTABLE.setMascara('myInput', callback);
+/* Asignación de máscara, utiliza eventos input y keyDown */
+MASCARA_CONTABLE.setMascara("myInput", imprimirPantalla)
+MASCARA_CONTABLE.setMascara("myOtherInput", calcularCambio, "10.00")
 
 
-MASCARA_CONTABLE.setMascara('myOtherInput',callback);
+
+/* Para resetear un elemento a su valor inicial(cuando se le aplicó la máscara) */
+
+// Resetea a 0.0.
+MASCARA_CONTABLE.clearElemento('myInput');
+// Resetea a 10.00
+MASCARA_CONTABLE.clearElemento('myOtherInput');
+
+/* Para resetear un elemento a un nuevo valor inicial */
+
+// Resetea a 10.0.
+MASCARA_CONTABLE.clearElemento('myInput','10.00');
+// Resetea a 0.00
+MASCARA_CONTABLE.clearElemento('myOtherInput', '0.00');
+
+
+document.getElementById('myInput').focus();
