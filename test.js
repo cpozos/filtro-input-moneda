c13863d9047415fb86a807cbef841b726e2b759f
cpozos <38520814+cpozos@users.noreply.github.com>
2019-07-15 18:01:02 -0500

    Mejora test cliente.
    Se usa la nueva función clearElemento

--- test.js
+++ test.js
@@ -1,8 +1,32 @@
 
+var toggle = 0;
+function cambiarfocus()
+{
+    switch(toggle){
+        case 0:
+            document.getElementById('otro').focus();  
+            break;
+
+        case 1:
+            MASCARA_CONTABLE.clearElemento('myInput');    
+            document.getElementById('myInput').focus();  
+            break;
+
+        case 2:
+            MASCARA_CONTABLE.clearElemento('myOtherInput');
+            document.getElementById('myOtherInput').focus();  
+            break;
+    }
+
+    toggle++;
+
+    toggle = toggle>2? 0 : toggle;
+}
+
+
 /* CALLBACKS */
 // Funciones que se ejecuta cuando existe un cambio en el valor del input
 // Se les pasa como parámetro el nuevo valor en string.
-
 function calcularCambio(nuevoValorEnInput)
 {
     console.log('Calculando cambio - valor: ' + nuevoValorEnInput );
@@ -16,8 +40,6 @@ function imprimirPantalla(nuevoValorEnInput)
 /* Asignación de máscara, utiliza eventos input y keyDown */
 MASCARA_CONTABLE.setMascara("myInput", imprimirPantalla)
 MASCARA_CONTABLE.setMascara("myOtherInput", calcularCambio, "10.00")
-
-
 
 /* Para resetear un elemento a su valor inicial(cuando se le aplicó la máscara) */
 
@@ -34,4 +56,3 @@ MASCARA_CONTABLE.clearElemento('myInput','10.00');
 MASCARA_CONTABLE.clearElemento('myOtherInput', '0.00');
 
 
-document.getElementById('myInput').focus();
